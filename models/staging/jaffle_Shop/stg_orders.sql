373349f4143040703c7ebbf79feb0f770f8aac44
Leslie Potgieter <Leslie.potgieter@vantagedata.co.za>
2020-12-15 15:07:03 +0000

    tests complete

---
+++ models/staging/jaffle_Shop/stg_orders.sql
@@ -0,0 +1,14 @@
+with orders as (
+    
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from raw.jaffle_shop.orders
+    -- completed, shipped, returned, placed, return_pending
+)
+
+select * from orders
+
